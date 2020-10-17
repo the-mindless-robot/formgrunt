5edd47d63d29150af63dc4e367406bfb0f1d3e6e
The Mindless Robot <joelarias@protonmail.com>
2020-10-16 19:02:26 -0700

    v2.0 - backwards compatible

--- FormGrunt.js
+++ FormGrunt.js
@@ -1,11 +1,179 @@
 class FormGrunt {
+
+    constructor(inputClass) {
+        this.inputClass = `.${inputClass}`;
+        this.inputs = document.querySelectorAll(this.inputClass);
+        this.inputCountByType = {};
+        this.invalidInputsArray = [];
+
+        this.inputList = this._initInputs(this.inputs);
+    }
+
+    _initInputs(inputs) {
+        const inputList = {};
+
+        for(const input of inputs) {
+
+            const inputAttributes = input.dataset;
+            // console.debug(inputAttributes);
+
+             //type is required
+             const type = inputAttributes.type ? inputAttributes.type.toLowerCase() : false;
+             const lookup = inputAttributes.lookup ? inputAttributes.lookup : false;
+
+             if(type && lookup) {
+                 this._addToCount(type);
+                 const elemId = input.id ? input.id : 'none';
+
+                 inputList[lookup] = {
+                     type,
+                     elemId,
+                     elem: input
+                 };
+             } else {
+                 this.invalidInputsArray.push(input);
+             }
+        }
+        console.debug('inputs', inputList, this.inputs);
+        return inputList;
+    }
+
+    _addToCount(type) {
+        if(this.inputCountByType.hasOwnProperty(type)) {
+            this.inputCountByType[type] += 1;
+        } else {
+            this.inputCountByType[type] = 1;
+        }
+    }
+
+    _createId(type) {
+        return `${type}${this.inputCountByType[type]}`;
+    }
+
+    getAllInputValues(stringFormat = false) {
+        console.log('getting values',this.inputList);
+        const allValues = {};
+        for(const lookup in this.inputList) {
+            console.log('getting values for', lookup);
+            const input = this.inputList[lookup];
+            const inputValue = stringFormat ? this._getInputValue(input, true) : this._getInputValue(input);
+
+            console.log('inputValue', inputValue);
+            allValues[lookup] = inputValue;
+        }
+
+        return allValues;
+    }
+
+    _getInputValue(input, stringFormat = false) {
+        const type = input.type;
+        let inputValue;
+
+        if (type == 'radio') {
+            inputValue = this._getSelectedRadioValue(input.elem);
+            if (stringFormat) {
+                return inputValue.value;
+            }
+            return inputValue;
+        }
+
+        if (type == 'text') {
+            inputValue = this._getTextInputValue(input.elem);
+            return inputValue;
+        }
+
+        if (type == 'select') {
+            inputValue = this._getSelectInputValues(input.elem);
+            if (stringFormat) {
+                return inputValue.toString;
+            }
+            return inputValue;
+        }
+
+
+        return 'invalid type';
+
+    }
+
+    _getSelectedRadioValue(elem) {
+        // const options = document.getElementsByName(name);
+        const options = elem.querySelectorAll('input[type="radio"]');
+        console.debug('options', elem, options);
+        let value = null;
+        let label = null;
+        for(let i=0;i < options.length; i++) {
+            if(options[i].checked) {
+                value = options[i].value;
+                label = options[i].nextElementSibling.innerText;
+            }
+        }
+        return {label, value};
+    }
+
+    _getTextInputValue(elem) {
+        if(elem.nodeName == "INPUT") {
+            return elem.value;
+        }
+        const input = elem.querySelector('input[type="text"]');
+        return input.value;
+    }
+
+    _getSelectInputValues(elem, hasPlaceholder = true) {
+        const values = {
+            toString: '',
+            toArray: [],
+            obj: {},
+            hasValues: false
+        };
+
+        let input;
+        if(elem.nodeName == "SELECT") {
+            input = elem;
+        } else {
+            input = elem.querySelector('select');
+        }
+
+        const options = input.options;
+        // console.log(options);
+        const start = hasPlaceholder ? 1 : 0;
+        let selected = 0;
+
+        for (let i=start; i<options.length; i++) {
+            // console.log(options[i]);
+            if (options[i].selected === true) {
+                // array
+                values.toArray.push(options[i].value);
+                //obj
+                values.obj[this._cleanLabel(options[i].value)] = true;
+                //string
+                if (selected !== 0) {
+                    values.toString += ', '+options[i].value;
+                } else {
+                    values.toString += options[i].value;
+                }
+                selected++;
+            } else {
+                values.obj[this._cleanLabel(options[i].value)] = false;
+            }
+
+        }
+        if(selected > 0) {
+            values.hasValues = true;
+        }
+        return values;
+    }
+
     // remove special chars/spaces
-    static cleanLabel(label) {
+    _cleanLabel(label) {
         let cleanLabel = label.replace('/', '-');
         cleanLabel = cleanLabel.split(' ').join('_');
         return cleanLabel;
     }
 
+
+    /*
+    **save for backwards compatibility with old applications**
+    */
     // get input from text field by element id
     static getTextInputValue(id) {
         return document.getElementById(id).value;
@@ -31,7 +199,7 @@ class FormGrunt {
                 // array
                 values.toArray.push(options[i].value);
                 //obj
-                values.obj[FormGrunt.cleanLabel(options[i].value)] = true;
+                values.obj[this._cleanLabel(options[i].value)] = true;
                 //string
                 if (selected !== 0) {
                     values.toString += ', '+options[i].value;
@@ -40,7 +208,7 @@ class FormGrunt {
                 }
                 selected++;
             } else {
-                values.obj[FormGrunt.cleanLabel(options[i].value)] = false;
+                values.obj[this._cleanLabel(options[i].value)] = false;
             }
 
         }
